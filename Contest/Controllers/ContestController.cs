d483349f9967a6be4bd219d7baa0d31f4e294b20
Tyler M <t_mckenzie@outlook.com>
2022-09-12 13:09:08 -0500

    Add project files.

---
+++ Contest/Controllers/ContestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contest.Controllers
+{
+    public class ContestController : Controller
+    {
+        public IActionResult Contest()
+        {
+            return View();
+        }
+    }
+}
